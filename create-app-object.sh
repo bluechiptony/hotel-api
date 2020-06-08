594ba9c65cdf1a95988d7f4346a8f6ad0bead6c6
bluechiptony <tony5egwu@gmail.com>
2020-06-08 21:28:55 +0100

    initial commit

---
+++ create-app-object.sh
@@ -0,0 +1,10 @@
+touch src/apidoc/$1.js &&
+touch src/routes/$1.ts &&
+mkdir src/handlers/$1 &&
+touch src/handlers/$1/$1.ts &&
+touch src/handlers/$1/$1.model.ts &&
+touch src/handlers/$1/$1-data-access.ts &&
+touch src/handlers/$1/$1-validator.ts &&
+touch src/handlers/$1/$1-adapter.ts &&
+touch src/handlers/$1/$1-use-cases.ts &&
+touch src/handlers/$1/$1.test.ts
